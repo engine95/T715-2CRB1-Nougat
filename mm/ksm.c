d05001cfd7e367d490c8bde892c31320df3324de
engine95 <mustangsroc@gmail.com>
2018-06-23 10:41:51 -0600

    adjust tweak for KSM

--- mm/ksm.c
+++ mm/ksm.c
@@ -218,7 +218,7 @@ static unsigned long ksm_pages_unshared;
 static unsigned long ksm_rmap_items;
 
 /* Number of pages ksmd should scan in one batch */
-static unsigned int ksm_thread_pages_to_scan = 256;
+static unsigned int ksm_thread_pages_to_scan = 100;
 
 /* Milliseconds ksmd should sleep between batches */
 static unsigned int ksm_thread_sleep_millisecs = 1100;
